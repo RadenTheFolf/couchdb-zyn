30e637d559a5c98a153244d63d7fc327799bd5b6
RadenTheFolf <radenthefolf@gmail.com>
2024-01-12 13:42:10 -0500

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='couchdb-zyn',
+    version='0.0.1',
+    packages=['couchdb-zyn'],
+    url='',
+    license='GNU GPLv3',
+    author='raden',
+    author_email='raden@zynosstudios.com',
+    description='CouchDB API wrapper'
+)
